29d007a7b99668f9fc18d6602f4dd74c6360fb92
spenseay <spenseay@stanford.edu>
2025-03-19 09:44:09 -0700

    deleted everything we're gonna start over with preston lol

--- src/main.js
+++
@@ -1,9 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import './style.css'
-
-// Create the Vue application
-const app = createApp(App)
-
-// Mount the app to the #app element
-app.mount('#app')
